10ffdf5c9acf26b57c770bc1318e7a890a009e2b
DangerN <cdneal091@gmail.com>
2019-03-13 15:52:45 -0600

    last commit

--- lib/person.rb
+++ lib/person.rb
@@ -50,19 +50,41 @@ class Person
 
   def get_paid(salary_amount)
     @bank_account += salary_amount
-    returns "all about the benjamins"
+    return "all about the benjamins"
   end
 
   def take_bath
+    # binding.pry
+    # hygiene= (self.hygiene + 4)
+    self.hygiene=(@hygiene + 4)
+    return '♪ Rub-a-dub just relaxing in the tub ♫'
   end
 
   def work_out
+    self.happiness=(@happiness + 2)
+    self.hygiene=(@hygiene - 3)
+    return '♪ another one bites the dust ♫'
   end
 
-  def call_friend
+  def call_friend(person)
+    self.happiness=(@happiness + 3)
+    person.happiness=(person.happiness + 3)
+    return "Hi #{person.name}! It's #{@name}. How are you?"
   end
 
   def start_conversation(person, topic)
+    case topic
+    when 'politics'
+      self.happiness=(@happiness - 2)
+      person.happiness=(person.happiness - 2)
+      return 'blah blah partisan blah lobbyist'
+    when 'weather'
+      self.happiness=(@happiness + 1)
+      person.happiness=(person.happiness + 1)
+      return 'blah blah sun blah rain'
+    else
+      return 'blah blah blah blah blah'
+    end
   end
 
 
